41bd2083a9f2aa91535a226e5077af0ea912d69c
Vitor Wdson <vitor.wdson2@gmail.com>
2024-05-18 16:22:42 -0400

    feat: initial ast structure

---
+++ ast/expressions.go
@@ -0,0 +1,11 @@
+package ast
+
+import "github.com/vitorwdson/monkey-interpreter/token"
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
