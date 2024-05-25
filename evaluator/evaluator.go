c44f28506ebb0ed1194f035a4f6f8c6fb8b1b54a
Vitor Wdson <vitor.wdson2@gmail.com>
2024-05-25 16:50:57 -0400

    feat: eval sum, sub, mul and div

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 	return nil
 }
---
+++ evaluator/infix.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import "github.com/vitorwdson/monkey-interpreter/object"
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	if left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ {
+		return evalIntegerInfixExpression(operator, left, right)
+	}
+
+	return NULL
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	}
+
+	return NULL
+}
